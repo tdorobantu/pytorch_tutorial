78ea067197d4aeadc45495ef810644a5897ae61e
Tudor Dorobantu <tdorobantu95@gmail.com>
2020-06-12 19:58:46 +0100

    commiting conv_net and MNIST notebooks

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,7 +1,20 @@
 import torch
 
-def hello_world():
-    print('hello_world')
+x = torch.tensor([5, 3])
+y = torch.tensor([2, 1])
 
-hello_world()
+print(x*y)
 
+x = torch.zeros([2, 5])
+
+print(x)
+
+print(x.shape)
+
+y = torch.rand([2, 5])
+
+print(y)
+
+y = y.view([1, 10])
+
+print(y)
